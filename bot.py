500729c4ecd6b3534ec35bc8a20dab447ff45e48
Krushovice <Kickstar69@yandex.ru>
2024-01-02 12:34:20 +0000

    Создан скилет бота

---
+++ bot.py
@@ -0,0 +1,42 @@
+import asyncio
+import logging
+import sys
+from config_data.config_reader import Config, load_config
+
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from handlers import user_handlers
+
+# Bot token can be obtained via https://t.me/BotFather
+logger = logging.getLogger(__name__)
+
+
+async def main() -> None:
+    # Конфигурируем логирование
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(filename)s:%(lineno)d #%(levelname)-8s '
+               '[%(asctime)s] - %(name)s - %(message)s')
+
+    # Выводим в консоль информацию о начале запуска бота
+    logger.info('Starting bot')
+
+    # Загружаем конфиг в переменную config
+    config: Config = load_config()
+
+    dp = Dispatcher()
+    bot = Bot(token=config.tg_bot.token,
+              parse_mode=ParseMode.HTML)
+    # Регистриуем роутеры в диспетчере
+    dp.include_router(user_handlers.router)
+    # dp.include_router(other_handlers.router)
+
+    # Пропускаем накопившиеся апдейты и запускаем polling
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,
+                        stream=sys.stdout)
+    asyncio.run(main())
---
+++ handlers/user_handlers.py
@@ -0,0 +1,57 @@
+from aiogram.filters import Command, CommandStart
+from aiogram.types import Message
+from aiogram import F, Router, types
+from keyboards.markups import yes_no_kb, profile_kb
+from lexicon.lexicon_data import LEXICON_RU
+
+
+router = Router()
+
+
+@router.message(CommandStart())
+async def command_start_handler(message: Message):
+    await message.answer(text=LEXICON_RU['/start'], reply_markup=yes_no_kb)
+
+
+@router.message(Command('help'))
+async def command_help_handler(message: Message):
+    await message.answer(text=LEXICON_RU['/help'], reply_markup=yes_no_kb)
+
+
+@router.message(Command('profile'))
+async def show_profile_handler(message: Message):
+    await message.answer(text=LEXICON_RU['/profile'], reply_markup=profile_kb)
+
+
+@router.message(F.text == LEXICON_RU['yes_button'])
+async def process_yes_answer(message: Message):
+    await message.answer(text=LEXICON_RU['yes'])
+
+
+@router.message(F.text == LEXICON_RU['no_button'])
+async def process_no_answer(message: Message):
+    await message.answer(text=LEXICON_RU['no'])
+
+
+@router.message(F.text)
+async def process_save_answer(message: Message):
+    msg = message.text.split(', ')
+    if len(msg) < 5:
+        await message.answer(text=LEXICON_RU['not_enough'])
+    else:
+        await message.answer(text=LEXICON_RU['save_books'])
+
+
+@router.message(F.text == LEXICON_RU['wish_list'])
+async def show_wish_list(message: Message):
+    pass
+
+
+@router.message(F.text == LEXICON_RU['check_list'])
+async def show_check_list(message: Message):
+    pass
+
+
+@router.message(F.text == LEXICON_RU['category'])
+async def show_category(message: Message):
+    pass
